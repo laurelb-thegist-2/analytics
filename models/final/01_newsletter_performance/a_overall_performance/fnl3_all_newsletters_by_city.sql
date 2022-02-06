5df8e45c2cb9ca3226d614cf6c733239263d1ba6
Laurel Buchanan <laurelb@thegistsports.com>
2022-02-06 16:30:47 +0000

    separated out special edition and regular newsletters from the newsletter performance by date/country/city queries

---
+++ models/final/01_newsletter_performance/a_overall_performance/fnl3_all_newsletters_by_city.sql
@@ -0,0 +1,20 @@
+with special_edition as (
+    select * 
+    from {{ref('fnl3a_special_edition')}}
+),
+
+regular as (
+    select * 
+    from {{ref('fnl3b_regular')}}
+),
+
+all_newsletters as (
+    select *
+    from special_edition
+    union
+    select * 
+    from regular
+)
+
+select * from all_newsletters 
+ORDER BY 1
