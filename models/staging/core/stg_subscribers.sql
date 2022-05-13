ec3aee78fcc7090d8af5df5e2107fc2ca69c8c0f
Laurel Buchanan <laurelb@thegistsports.com>
2022-05-13 10:48:49 +0000

    added tests to core files

--- models/staging/core/stg_subscribers.sql
+++ models/staging/core/stg_subscribers.sql
@@ -4,7 +4,7 @@ with subscribers as (
         LEADID,
         LISTID as list_ID,
         cast("DATE" as DATE) date_status_changed,
-        STATUS,
+        STATUS as Status,
         coalesce(GROWTHCHANNEL, 'Organic/Unknown') as Growth_Channel,
         CASE WHEN Growth_Channel ilike '%dojo%' or Growth_Channel ilike '%contest%' or Growth_Channel ilike '%coreg%' or Growth_Channel ilike '%co-reg%' or Growth_Channel ilike '%Scholarship%' or Growth_Channel ilike '%socialstance%' or Growth_Channel ilike '%social stance%' or Growth_Channel ilike '%PLN%' THEN 'Incentivized' ELSE 'Unincentivized' END Incentivization,
         COUNTRY as country,
@@ -14,7 +14,7 @@ with subscribers as (
         CAMPAIGNNAME as campaign_name,
         SOURCEBRAND as source_brand,
         PARTNERENGAGEMENTSURVEYS as Partner_Engagement_Surveys
-    from {{ source('core', 'subscribers') }}
+    from {{ source('core', 'all_subscribers') }}
     where list_id = '54eb7610971ecdad5354d8d07b2b6397' --sports news
     --where list_id = '65ef2f913391ff42878e99dd01601196' --sports biz
 )
